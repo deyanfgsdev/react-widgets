9e835b0bf516f28282d644a6e46bfdba148a1a2c
Deyán G. Sabogal <deyanfabricioguacaransabogal@MacBook-Pro-de-Deyan-Fabricio-Guacaran-Sabogal.local>
2020-07-14 11:21:33 +0100

    Add delayed request with setTimeout on Search Component

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,10 +20,33 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    if (term) {
-      search();
-    }
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -37,6 +60,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
